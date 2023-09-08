f95b7e176c6f6a7d55367d59794fea918b8fb2c3
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-08 13:45:11 -0600

    Delete post request

--- Cheeseaholics/server/services/PostService.js
+++ Cheeseaholics/server/services/PostService.js
@@ -2,6 +2,9 @@ import { dbContext } from "../db/DbContext.js";
 import { BadRequest } from "../utils/Errors.js";
 
 class PostService {
+    deletePost(postId) {
+
+    }
     async createPost(body) {
         let newPost = await dbContext.Post.create(body)
         return newPost
