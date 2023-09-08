1bc86ff493c6ebb8d39ec383ed89b2479ed7c704
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-08 14:51:58 -0600

    Added changes

--- Cheeseaholics/server/models/Comment.js
+++ Cheeseaholics/server/models/Comment.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
-
+import { Schema } from "mongoose"
+import { Mongoose } from "mongoose"
 export const CommentSchema = new Schema(
     {
         votes: { type: Number, required: true, default: 0 },
-        description: { type: String, required: true, maxlength: 100, minlength: 4 }
+        description: { type: String, required: true, maxlength: 100, minlength: 4 },
+        authorId: { type: Schema.Types.ObjectId, required: true, ref: "profileId" }
     },
     { timestamps: true, toJSON: { virtuals: true } }
 )
@@ -11,6 +12,13 @@ export const CommentSchema = new Schema(
 CommentSchema.virtual('profileId', {
     localField: 'authorId',
     ref: 'Account',
-    foreignField: 'id',
+    foreignField: '_id',
+    justOne: true
+})
+
+CommentSchema.virtual('postId', {
+    localField: 'authorId',
+    ref: 'Post',
+    foreignField: '_id',
     justOne: true
 })
--- Cheeseaholics/server/models/Vote.js
+++ Cheeseaholics/server/models/Vote.js
@@ -1,16 +1,24 @@
-import mongoose from 'mongoose'
+import { Schema } from 'mongoose'
 
 export const VoteSchema = new Schema(
     {
         vote: { type: Boolean },
 
-        authorId: { type: Schema.Types.ObjectId, required: true, ref: 'VoteId' }
+        authorId: { type: Schema.Types.ObjectId, required: true, ref: 'profileId' },
+        parentId: { type: Schema.Types.ObjectId, required: true }
     }, { timestamps: true, toJSON: { virtuals: true } }
 )
 
 VoteSchema.virtual('profileId', {
     localField: 'authorId',
     ref: 'Account',
+    foreignField: 'id',
+    justOne: true
+})
+
+VoteSchema.virtual('virtualParentId', {
+    localField: 'parentId',
+    ref: 'Post' || 'Comment',
     foreignField: 'id',
     justOne: true
 })
