728b5bd313eecd148b700c4311373def809617af
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-08 13:47:07 -0600

    Delete HTTP request for POSTS complete

--- Cheeseaholics/server/services/PostService.js
+++ Cheeseaholics/server/services/PostService.js
@@ -2,8 +2,11 @@ import { dbContext } from "../db/DbContext.js";
 import { BadRequest } from "../utils/Errors.js";
 
 class PostService {
-    deletePost(postId) {
-
+    async deletePost(postId) {
+        let postToDelete = await dbContext.Post.findById(postId)
+        if (!postToDelete) throw new BadRequest('404 Invalid Request.')
+        postToDelete.remove()
+        return `POST => ${postToDelete.description} <= HAS BEEN REMOVED`
     }
     async createPost(body) {
         let newPost = await dbContext.Post.create(body)
