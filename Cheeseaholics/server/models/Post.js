dbbb16a61714324fc30842e7ffaa2e72919b21c1
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-09-08 14:05:08 -0600

    added pfp

--- Cheeseaholics/server/models/Post.js
+++ Cheeseaholics/server/models/Post.js
@@ -6,7 +6,8 @@ export const PostSchema = new Schema({
     cheeseType: { type: String, enum: ['cheddar', 'mozzarella', 'american', 'parmesan', 'bleuCheese'] },
     votes: { type: Number, required: true, default: 0 },
     authorId: { type: Schema.Types.ObjectId, required: true, ref: 'profileId' },
-    authorName: { type: Schema.Types.String, required: true, ref: 'profileName' }
+    authorName: { type: Schema.Types.String, required: true, ref: 'profileName' },
+    authorPicture: { type: Schema.Types.String, required: true, ref: 'profilePicture' }
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 PostSchema.virtual('profileId', {
